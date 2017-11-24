242d5690aeb9d906fb5a7abcb7c28d0b2dbc96ce
Protinam <protinam@protonmail.ch>
2017-11-24 13:35:36 -0800

    Exchange contract prototype

--- contracts/exchange/Exchange.sol
+++ contracts/exchange/Exchange.sol
@@ -1,17 +1,8 @@
 /*
 
-  << Project Wyvern Exchange >>
-
-  Trustless decentralized digital item exchange.
+  Trustless decentralized digital item & smart contract exchange.
 
   Written from scratch.
-
-*/
-
-/*
-
-  TODO:
-    - Think about auction format, gas price races
 
 */
 
@@ -36,7 +27,7 @@ contract Exchange is Ownable {
   /* The fee required to buy an item. */
   uint public feeBuy;
 
-  /* Current collected fees. */
+  /* Current collected fees, reset upon withdrawal. */
   uint public collectedFees;
 
   /* The token used to pay exchange fees. */
@@ -51,9 +42,6 @@ contract Exchange is Ownable {
   /* All sales that have ever occurred. */
   mapping(bytes32 => Sale) public sales;
 
-  /* Metadata for all completed sales. */
-  mapping(bytes32 => SaleMetadata) public saleMetadata;
-
   /* Top bids for all English auctions. */
   mapping(bytes32 => Bid) public topBids;
 
@@ -66,11 +54,8 @@ contract Exchange is Ownable {
     address seller;
     /* Item contract - a value of 0 means no contract is being sold. */
     Claimable contractAddress;
-    /* Item metadata - hash of IPFS file and kind of hash used. */
-    bytes32 metadataHash;
-    uint8 metadataKind;
-    uint8 metadataSize;
-    /* TODO: Hash to link to encrypted secret after sale completed. */
+    /* Item metadata IPFS hash. */
+    bytes metadataHash;
     /* The kind of sale. */
     SaleKind saleKind;
     /* Token used to pay for the item. */
@@ -81,8 +66,8 @@ contract Exchange is Ownable {
     uint listingTime;
     /* Expiration timestamp - 0 for no expiry. */
     uint expirationTime;
-    /* Decay factor, see documentation. */
-    uint decayFactor;
+    /* Auction extra parameter - minimum bid increment for English auctions, decay factor for Dutch auctions. */
+    uint auctionExtra;
     /* The escrow provider contract, which is paid a fee to arbitrage escrow disputes and must provide (TODO) specific functions. 0 for no escrow. */
     EscrowProvider escrowProvider;
     /* Whether or not the listing has been removed. */
@@ -90,10 +75,10 @@ contract Exchange is Ownable {
   }
 
   struct Bid {
-    /* Amount of the bid. */
-    uint amount;
     /* Address of the bidder. */
     address bidder;
+    /* Amount of the bid. */
+    uint amount;
     /* Timestamp of bid placement. */
     uint timestamp;
   }
@@ -105,13 +90,15 @@ contract Exchange is Ownable {
     uint price;
     /* Timestamp of purchase. */
     uint timestamp;
+    /* Sale metadata IPFS hash. */
+    bytes metadataHash;
   }
 
-  struct SaleMetadata {
-    bytes32 hash;
-    uint8 kind;
-    uint8 size;
-  }
+  event ItemListed    (bytes32 id, address seller, Claimable contractAddress, bytes metadataHash, SaleKind saleKind, ERC20 paymentToken, uint price, uint timestamp, uint expirationTime, uint auctionExtra, EscrowProvider escrowProvider);
+  event ItemRemoved   (bytes32 id);
+  event ItemBidOn     (bytes32 id, address bidder, uint amount, uint timestamp);
+  event ItemPurchased (bytes32 id, address buyer, uint price);
+  event SaleFinalized (bytes32 id, bytes metadataHash);
 
   modifier costs (uint amount) {
     require(exchangeTokenAddress.transferFrom(msg.sender, this, amount));
@@ -122,51 +109,110 @@ contract Exchange is Ownable {
   modifier requiresActiveItem (bytes32 id) {
     require(
       (items[id].seller != address(0)) &&
-      (!items[id].removed)
+      (!items[id].removed) &&
+      (sales[id].buyer == address(0))
     );
     _;
   }
 
-  function listItem(Claimable contractAddress, bytes32 itemMetadataHash, uint8 itemMetadataKind, uint8 itemMetadataSize, SaleKind saleKind, ERC20 paymentToken, uint price, uint expirationTime, uint decayFactor, EscrowProvider escrowProvider) costs (feeList) returns (bytes32 id) {
-    /* TODO: Think about preventing duplicates. */
-    id = sha3(msg.sender, contractAddress, itemMetadataHash, itemMetadataKind, itemMetadataSize, saleKind, paymentToken, price, now, expirationTime, decayFactor, escrowProvider);
+  function withdrawFees(address dest) onlyOwner {
+    require(exchangeTokenAddress.transfer(dest, collectedFees));
+    collectedFees = 0;
+  }
+
+  function setListFee(uint listFee) onlyOwner {
+    feeList = listFee;
+  }
+
+  function setBidFee(uint bidFee) onlyOwner {
+    feeBid = bidFee;
+  }
+
+  function setBuyFee(uint buyFee) onlyOwner {
+    feeBuy = buyFee;
+  }
+
+  function listItem(Claimable contractAddress, bytes metadataHash, SaleKind saleKind, ERC20 paymentToken, uint price, uint expirationTime, uint auctionExtra, EscrowProvider escrowProvider) costs (feeList) returns (bytes32 id) {
+    id = sha3(msg.sender, contractAddress, metadataHash, saleKind, paymentToken, price, now, expirationTime, auctionExtra, escrowProvider);
     require(items[id].seller == address(0));
     if (contractAddress != address(0)) {
       contractAddress.claimOwnership();
     }
-    items[id] = Item(msg.sender, contractAddress, itemMetadataHash, itemMetadataKind, itemMetadataSize, saleKind, paymentToken, price, now, expirationTime, decayFactor, escrowProvider, false);
+    items[id] = Item(msg.sender, contractAddress, metadataHash, saleKind, paymentToken, price, now, expirationTime, auctionExtra, escrowProvider, false);
     ids.push(id);
+    ItemListed(id, msg.sender, contractAddress, metadataHash, saleKind, paymentToken, price, now, expirationTime, auctionExtra, escrowProvider);
     return id;
   }
 
-  function removeItem (bytes32 id) {
-    require(
-      (items[id].seller == msg.sender) &&
-      (!items[id].removed)
-      );
+  function removeItem (bytes32 id) requiresActiveItem(id) {
+    require(items[id].seller == msg.sender);
     items[id].removed = true;
+    ItemRemoved(id);
   }
 
-  function bidOnItem (bytes32 id) requiresActiveItem (id) costs (feeBid) {
-    require(items[id].saleKind == SaleKind.EnglishAuction);
-    /* TODO */
+  function bidOnItem (bytes32 id, uint amount) requiresActiveItem (id) costs (feeBid) {
+    /* Must be an English auction that has not yet completed. */
+    require(
+      (items[id].saleKind == SaleKind.EnglishAuction) &&
+      (now < items[id].expirationTime)
+      );
+    if (topBids[id].bidder == address(0)) {
+      /* Amount must be at least the minimum bid. */
+      require(amount >= items[id].basePrice);
+    } else {
+      /* Amount must be at least the last high bid plus the minimum bid increment. */
+      require(amount >= topBids[id].amount + items[id].auctionExtra);
+      /* Return locked tokens to the previous high bidder. */
+      require(items[id].paymentToken.transfer(topBids[id].bidder, topBids[id].amount));
+    }
+    /* Lock tokens for the new high bidder. */
+    require(items[id].paymentToken.transferFrom(msg.sender, this, amount));
+    /* Store the new high bid. */
+    topBids[id] = Bid(msg.sender, amount, now);
+    /* Log the bid event. */
+    ItemBidOn(id, msg.sender, amount, now);
   }
 
-  /* Called by a buyer to purchase an item */
+  function calculateFinalItemPrice (bytes32 id) public constant requiresActiveItem(id) returns (uint price) {
+    if (items[id].saleKind == SaleKind.FixedPrice) {
+      return items[id].basePrice;
+    } else if (items[id].saleKind == SaleKind.EnglishAuction) {
+      require(topBids[id].bidder != address(0));
+      return topBids[id].amount;
+    } else if (items[id].saleKind == SaleKind.DutchAuction) {
+      return items[id].basePrice * (1 - (items[id].auctionExtra * (now - items[id].listingTime) / (items[id].expirationTime - items[id].listingTime)));
+    } else {
+      throw;
+    }
+  }
+
+  /* Called by a buyer to purchase an item or finalize an auction they have won. */
   function purchaseItem (bytes32 id) requiresActiveItem (id) costs (feeBuy) {
-    /* TODO Deal with English auction, calculate price for Dutch auction. */
-    uint price = items[id].basePrice;
+    if (items[id].saleKind == SaleKind.EnglishAuction) {
+      Bid topBid = topBids[id];
+      require(
+        (msg.sender == topBid.bidder) &&
+        (now >= items[id].expirationTime)
+      );
+      items[id].paymentToken.transfer(msg.sender, topBid.amount);
+    } else {
+      require(now < items[id].expirationTime);
+    }
+    uint price = calculateFinalItemPrice(id);
     require(items[id].escrowProvider.holdInEscrow(id, msg.sender, items[id].seller, items[id].paymentToken, price));
     Claimable contractAddress = items[id].contractAddress;
     if (contractAddress != address(0)) {
       contractAddress.transferOwnership(msg.sender);
     }
-    sales[id] = Sale(msg.sender, price, now); 
+    sales[id] = Sale(msg.sender, price, now, new bytes(0));
+    ItemPurchased(id, msg.sender, price);
   }
 
-  /* Called by the seller of an item to finalize sale to the buyer. */
-  function finalizeSale (bytes32 id, bytes32 saleMetadataHash, uint8 saleMetadataKind, uint8 saleMetadataSize) {
-    /* TODO */
+  /* Called by the seller of an item to finalize sale to the buyer, linking the IPFS metadata. */
+  function finalizeSale (bytes32 id, bytes saleMetadataHash) {
+    require(items[id].seller == msg.sender);
+    sales[id].metadataHash = saleMetadataHash;
+    SaleFinalized(id, saleMetadataHash);
   }
 
 }
