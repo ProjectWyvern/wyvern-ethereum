b985532c6d99b755b0137fc3be0dde7dfc8a7335
Protinam <protinam@protonmail.ch>
2017-12-30 23:26:29 -0800

    Constant fix

--- contracts/WyvernDAO.sol
+++ contracts/WyvernDAO.sol
@@ -17,9 +17,9 @@ contract WyvernDAO is DelegatedShareholderAssociation {
     string public constant name = "Project Wyvern DAO";
 
     uint public constant TOKEN_DECIMALS                     = 18;
-    uint public constant REQUIRED_SHARES_TO_BE_BOARD_MEMBER = 2000 * (10 ** TOKEN_DECIMALS); // set to ~ 0.1% of supply
-    uint public constant MINIMUM_QUORUM                     = 2000000 * (10 ** TOKEN_DECIMALS); // set to 10% of supply
-    uint public constant DEBATE_PERIOD_MINUTES              = 60 * 24 * 3; // set to 3 days initially
+    uint public constant REQUIRED_SHARES_TO_BE_BOARD_MEMBER = 200 * (10 ** TOKEN_DECIMALS); // set to ~ 0.1% of supply
+    uint public constant MINIMUM_QUORUM                     = 200000 * (10 ** TOKEN_DECIMALS); // set to 10% of supply
+    uint public constant DEBATE_PERIOD_MINUTES              = 60 * 24 * 3; // set to 3 days
 
     function WyvernDAO (ERC20 sharesAddress) public {
         sharesTokenAddress = sharesAddress;
