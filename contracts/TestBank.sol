70dca158c459753aa2cf23eb116d5ef12d33d66a
Protinam <protinam@protonmail.ch>
2018-01-23 10:08:31 -0800

    TestBank tests contd.

--- contracts/TestBank.sol
+++ contracts/TestBank.sol
@@ -8,6 +8,8 @@ pragma solidity 0.4.18;
 
 import "./common/LazyBank.sol";
 
+import "zeppelin-solidity/contracts/token/ERC20.sol";
+
 /**
  * @title TestBank
  * @author Project Wyvern Developers
@@ -17,6 +19,36 @@ contract TestBank is LazyBank {
     string public constant name = "Test Bank";
 
     function TestBank () public {
+    }
+
+    function _credit(address user, ERC20 token, uint amount)
+        public
+    {
+        credit(user, token, amount);
+    }
+
+    function _lazyDebit(address user, ERC20 token, uint amount)
+        public
+    {
+        lazyDebit(user, token, amount);
+    }
+
+    function _transferTo(address from, address to, ERC20 token, uint amount)
+        public
+    {
+        transferTo(from, to, token, amount);
+    }
+    
+    function _lazyLock(address user, ERC20 token, uint amount)
+        public
+    {
+        lazyLock(user, token, amount);
+    }
+    
+    function _unlock(address user, ERC20 token, uint amount)
+        public
+    {
+        unlock(user, token, amount);
     }
 
 }
--- test/test-bank.js
+++ test/test-bank.js
@@ -40,6 +40,88 @@ contract('TestBank', (accounts) => {
     })
   })
 
+  it('should set correct balance, available, and locked after locking', () => {
+    const amount = new BigNumber(10).pow(25).mul(2)
+    return TokenAndBank(({ tokenInstance, bankInstance }) => {
+      return bankInstance._lazyLock(accounts[0], tokenInstance.address, amount).then(() => {
+        return bankInstance.balanceFor.call(accounts[0], tokenInstance.address).then(balance => {
+          assert.equal(balance.equals(amount), true, 'Balance was not set correctly after locking')
+        }).then(() => {
+          return bankInstance.availableFor.call(accounts[0], tokenInstance.address).then(available => {
+            assert.equal(available.equals(0), true, 'Available was not set correctly after locking')
+          }).then(() => {
+            return bankInstance.lockedFor(accounts[0], tokenInstance.address).then(locked => {
+              assert.equal(locked.equals(amount), true, 'Locked was not set correctly after locking')
+            })
+          })
+        })
+      })
+    })
+  })
+
+  it('should fail to lock tokens', () => {
+    const amount = new BigNumber(10).pow(25).mul(2)
+    return TokenAndBank(({ tokenInstance, bankInstance }) => {
+      return bankInstance._lazyLock(accounts[0], tokenInstance.address, amount).then(() => {
+        assert.equal(true, false, 'Token locking was allowed')
+      }).catch(err => {
+        assert.equal(err.message, 'VM Exception while processing transaction: revert', 'Incorrect error')
+      })
+    })
+  })
+
+  it('should fail to debit tokens', () => {
+    const amount = new BigNumber(10).pow(25).mul(2)
+    return TokenAndBank(({ tokenInstance, bankInstance }) => {
+      return bankInstance._lazyDebit(accounts[0], tokenInstance.address, amount).then(() => {
+        assert.equal(true, false, 'Token locking was allowed')
+      }).catch(err => {
+        assert.equal(err.message, 'VM Exception while processing transaction: revert', 'Incorrect error')
+      })
+    })
+  })
+
+  it('should allow debit after credit', () => {
+    const amount = new BigNumber(10).pow(25).mul(2)
+    return TokenAndBank(({ tokenInstance, bankInstance }) => {
+      return bankInstance._credit(accounts[0], tokenInstance.address, amount).then(() => {
+        return bankInstance._lazyDebit(accounts[0], tokenInstance.address, amount).then(() => {
+          return bankInstance.balanceFor.call(accounts[0], tokenInstance.address).then(balance => {
+            assert.equal(balance.equals(amount), true, 'Balance was not set correctly after credit-debit')
+          })
+        })
+      })
+    })
+  })
+
+  it('should allow unlock', () => {
+    const amount = new BigNumber(10).pow(25).mul(2)
+    return TokenAndBank(({ tokenInstance, bankInstance }) => {
+      return bankInstance._unlock(accounts[0], tokenInstance.address, amount).then(() => {
+        return bankInstance.lockedFor(accounts[0], tokenInstance.address).then(locked => {
+          assert.equal(locked, 0, 'Locked was nonzero after unlock')
+        })
+      })
+    })
+  })
+
+  it('should allow transferTo', () => {
+    const amount = new BigNumber(10).pow(25).mul(2)
+    return TokenAndBank(({ tokenInstance, bankInstance }) => {
+      return bankInstance._transferTo(accounts[0], accounts[1], tokenInstance.address, amount).then(() => {
+        return bankInstance.balanceFor(accounts[0], tokenInstance.address).then(balance => {
+          assert.equal(balance, 0, 'Balance was nonzero after transferTo')
+        }).then(() => {
+          return bankInstance.balanceFor(accounts[1], tokenInstance.address).then(balance => {
+            assert.equal(balance.equals(amount), true, 'Balance was incorrect after transferTo')
+          }).then(() => {
+            return bankInstance._transferTo(accounts[1], accounts[0], tokenInstance.address, amount)
+          })
+        })
+      })
+    })
+  })
+
   it('should set correct balance, available, and locked after withdrawal', () => {
     const amount = new BigNumber(10).pow(25).mul(2)
     return TokenAndBank(({ tokenInstance, bankInstance }) => {
