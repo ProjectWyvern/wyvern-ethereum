391e62d620f8e2d305ad1b6e4ae62ca03bede5a6
Protinam <protinam@protonmail.ch>
2018-01-29 14:08:13 -0800

    External calculateMatchPrice & lint fixes

--- contracts/exchange/Exchange.sol
+++ contracts/exchange/Exchange.sol
@@ -166,6 +166,31 @@ contract Exchange is ExchangeCore {
     }
 
     /* Solidity ABI encoding limitation workaround, hopefully temporary. */
+    function calculateMatchPrice_(
+        address[12] addrs,
+        uint[14] uints,
+        uint8[6] sidesKindsHowToCalls,
+        bytes calldataBuy,
+        bytes calldataSell,
+        bytes replacementPatternBuy,
+        bytes replacementPatternSell,
+        bytes metadataHashBuy,
+        bytes metadataHashSell)
+        public
+        view
+        returns (uint)
+    {
+        Order memory buy = Order(addrs[0], addrs[1], addrs[2], uints[0], uints[1], addrs[3], SaleKindInterface.Side(sidesKindsHowToCalls[0]), SaleKindInterface.SaleKind(sidesKindsHowToCalls[1]), addrs[4], AuthenticatedProxy.HowToCall(sidesKindsHowToCalls[2]), calldataBuy, replacementPatternBuy, metadataHashBuy, ERC20(addrs[5]), uints[2], uints[3], uints[4], uints[5], uints[6]);
+        Order memory sell = Order(addrs[6], addrs[7], addrs[8], uints[7], uints[8], addrs[9], SaleKindInterface.Side(sidesKindsHowToCalls[3]), SaleKindInterface.SaleKind(sidesKindsHowToCalls[4]), addrs[10], AuthenticatedProxy.HowToCall(sidesKindsHowToCalls[5]), calldataSell, replacementPatternSell, metadataHashSell, ERC20(addrs[11]), uints[9], uints[10], uints[11], uints[12], uints[13]);
+        SaleKindInterface.Bid storage topBid = topBids[hashOrder(sell)];
+        return calculateMatchPrice(
+          buy,
+          sell,
+          topBid
+        );
+    }
+
+    /* Solidity ABI encoding limitation workaround, hopefully temporary. */
     function atomicMatch_(
         address[12] addrs,
         uint[14] uints,
--- contracts/registry/AuthenticatedLazyBank.sol
+++ contracts/registry/AuthenticatedLazyBank.sol
@@ -15,52 +15,52 @@ import "./ProxyRegistry.sol";
  */
 contract AuthenticatedLazyBank is LazyBank {
 
-  ProxyRegistry public registry;
+    ProxyRegistry public registry;
 
-  modifier withRegistryAuth {
-      require(registry.contracts(msg.sender));
-      _;
-  }
+    modifier withRegistryAuth {
+        require(registry.contracts(msg.sender));
+        _;
+    }
 
-  function AuthenticatedLazyBank(ProxyRegistry addrRegistry)
-      public
-  {
-      registry = addrRegistry;
-  }
+    function AuthenticatedLazyBank(ProxyRegistry addrRegistry)
+        public
+    {
+        registry = addrRegistry;
+    }
 
-  function _credit(address user, ERC20 token, uint amount)
-      public
-      withRegistryAuth
-  {
-      credit(user, token, amount);
-  }
+    function _credit(address user, ERC20 token, uint amount)
+        public
+        withRegistryAuth
+    {
+        credit(user, token, amount);
+    }
 
-  function _lazyDebit(address user, ERC20 token, uint amount)
-      public
-      withRegistryAuth
-  {
-      lazyDebit(user, token, amount);
-  }
+    function _lazyDebit(address user, ERC20 token, uint amount)
+        public
+        withRegistryAuth
+    {
+        lazyDebit(user, token, amount);
+    }
 
-  function _transferTo(address from, address to, ERC20 token, uint amount)
-      public
-      withRegistryAuth
-  {
-      transferTo(from, to, token, amount);
-  }
+    function _transferTo(address from, address to, ERC20 token, uint amount)
+        public
+        withRegistryAuth
+    {
+        transferTo(from, to, token, amount);
+    }
 
-  function _lazyLock(address user, ERC20 token, uint amount)
-      public
-      withRegistryAuth
-  {
-      lazyLock(user, token, amount);
-  }
+    function _lazyLock(address user, ERC20 token, uint amount)
+        public
+        withRegistryAuth
+    {
+        lazyLock(user, token, amount);
+    }
 
-  function _unlock(address user, ERC20 token, uint amount)
-      public
-      withRegistryAuth
-  {
-      unlock(user, token, amount);
-  }
+    function _unlock(address user, ERC20 token, uint amount)
+        public
+        withRegistryAuth
+    {
+        unlock(user, token, amount);
+    }
 
 }
