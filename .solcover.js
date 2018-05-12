b31d099650f1749eb36a19aa727bdfe7115a7dde
Protinam <protinam@protonmail.ch>
2018-05-12 20:51:01 +0200

    Attempt coverage fix

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,7 @@
 module.exports = {
   port: 8545,
   copyPackages: ['zeppelin-solidity'],
-  skipFiles: ['common/ArrayUtils.sol']
+  skipFiles: ['common/ArrayUtils.sol'],
+  compileCommand: '../node_modules/.bin/truffle compile',
+  testCommand: '../node_modules/.bin/truffle test --network coverage'
 }
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf build
-./node_modules/solidity-coverage/bin/exec.js
+./node_modules/.bin/solidity-coverage
 cat coverage/lcov.info | ./node_modules/.bin/coveralls
