3ec8569ea4547ea8677e6e6fbdbb014c95aa7237
Protinam <protinam@protonmail.ch>
2018-05-16 16:01:18 +0200

    Skip TestStatic.sol

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 8545,
   copyPackages: ['openzeppelin-solidity'],
-  skipFiles: ['common/ArrayUtils.sol', '../openzeppelin-solidity/contracts/math/SafeMath.sol'],
+  skipFiles: ['TestStatic.sol', 'common/ArrayUtils.sol', '../openzeppelin-solidity/contracts/math/SafeMath.sol'],
   compileCommand: '../node_modules/.bin/truffle compile',
   testCommand: '../node_modules/.bin/truffle test --network coverage'
 }
