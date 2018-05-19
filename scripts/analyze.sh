7d54180ea9cde5e8d23e45a7cc31363233f067c1
Protinam <protinam@protonmail.ch>
2018-05-19 17:59:46 +0200

    Update flatten / analysis scripts

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -3,10 +3,11 @@
 yarn flatten
 
 alias oyente="docker run -v $(pwd):/opt luongnguyen/oyente /oyente/oyente/oyente.py"
+alias myth="docker run -v $(pwd):/opt mythril myth"
 
 for contract in $(ls temp/); do
   echo "Analyzing $contract with Mythril..."
-  myth -x temp/$contract
+  myth -x /opt/temp/$contract
   echo "Analyzing $contract with Oyente..."
   oyente -s /opt/temp/$contract
 done
--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -4,10 +4,10 @@ rm -rf temp
 mkdir -p temp
 
 cd contracts
-find . -type f -name "*.sol" -exec sed -i 's/pragma solidity 0.4.19/pragma solidity ^0.4.19/g' {} +
+find . -type f -name "*.sol" -exec sed -i 's/pragma solidity 0.4.23/pragma solidity ^0.4.23/g' {} +
 cd ..
 
-alias flatten="solidity_flattener --solc-paths zeppelin-solidity=$(pwd)/node_modules/zeppelin-solidity"
+alias flatten="solidity_flattener --solc-paths openzeppelin-solidity=$(pwd)/node_modules/openzeppelin-solidity"
 
 flatten contracts/WyvernAtomicizer.sol --output temp/WyvernAtomicizer.sol
 flatten contracts/WyvernTokenTransferProxy.sol --output temp/WyvernTokenTransferProxy.sol
@@ -18,5 +18,5 @@ flatten contracts/WyvernExchange.sol --output temp/WyvernExchangeFlattened.sol
 flatten contracts/WyvernProxyRegistry.sol --output temp/WyvernProxyRegistryFlattened.sol
 
 cd contracts
-find . -type f -name "*.sol" -exec sed -i 's/pragma solidity ^0.4.19/pragma solidity 0.4.19/g' {} +
+find . -type f -name "*.sol" -exec sed -i 's/pragma solidity ^0.4.23/pragma solidity 0.4.23/g' {} +
 cd ..
